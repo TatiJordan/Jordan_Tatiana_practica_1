8209ec86cb7325e116e843c27a8d6f15a7b2708e
TatiJordan <jordan_perez.tatiana@tiernoparla.es>
2022-12-01 23:32:40 +0100

    Creacion del mensaje

--- src/main/java/com/mycompany/jordan_tatiana_practica_1/Jordan_Tatiana_Practica1.java
+++ src/main/java/com/mycompany/jordan_tatiana_practica_1/Jordan_Tatiana_Practica1.java
@@ -2,5 +2,10 @@ package com.mycompany.jordan_tatiana_practica_1;
 
 public class Jordan_Tatiana_Practica1 {
     public static void main(String[] args) {
-    }//
+        
+        //creacion del mensaje
+        int[] msj = {1, 1, 1, 1, 1, 1, 1, 1};
+        int bParidad = 0;
+        
+    }//main
 }//Practica 1
