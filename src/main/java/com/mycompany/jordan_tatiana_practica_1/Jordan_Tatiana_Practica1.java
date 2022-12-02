f53b7c95617ea9363409880ee3393a7c12fd0875
TatiJordan <jordan_perez.tatiana@tiernoparla.es>
2022-12-02 02:00:23 +0100

    Comienzo del codigo

--- src/main/java/com/mycompany/jordan_tatiana_practica_1/Jordan_Tatiana_Practica1.java
+++ src/main/java/com/mycompany/jordan_tatiana_practica_1/Jordan_Tatiana_Practica1.java
@@ -1,6 +1,7 @@
 package com.mycompany.jordan_tatiana_practica_1;
 
 public class Jordan_Tatiana_Practica1 {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
+        
     }//
 }//Practica 1
